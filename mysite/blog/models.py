e13797a910b427d4040d9d88dbcadbc86e003b03
Evan Cope <copev313@gmail.com>
2021-04-12 19:51:39 -0400

    added some views and templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublisherManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublisherManager, self).get_queryset().filter(
+            status='published')
 
 
 class Post(models.Model):
 
-    STATUS_CHOICES = ( ('draft', 'Draft'),
-                       ('published', 'Published'),
-    )
+    STATUS_CHOICES = (('draft', 'Draft'),
+                      ('published', 'Published'))
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
@@ -22,6 +29,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublisherManager()
 
     class Meta:
         ordering = ('-publish', )
@@ -29,4 +38,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
