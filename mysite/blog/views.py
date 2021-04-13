ca82fb3fa80817e0f4cbff91eb947ebaee959f46
Evan Cope <copev313@gmail.com>
2021-04-12 20:23:52 -0400

    added pagination + beginning of styling

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    # Display three posts on each page:
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # When the page is not an integer, simple deliver the first page:
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If the page is out of range, then display the last page of results:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
