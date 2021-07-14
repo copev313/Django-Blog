78cfece3bf2895f116e6777d23d291d3e02960ad
Evan Cope <copev313@gmail.com>
2021-07-14 12:56:10 -0400

    added comments feature for posts

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-
+# Post Model:
 @admin.action(description="Mark selected blog posts as published")
 def make_published(modeladmin, request, queryset):
     queryset.update(status='published')
 
-
 @admin.action(description="Mark selected blog posts as drafts")
 def make_draft(modeladmin, request, queryset):
     queryset.update(status='draft')
-    
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -22,4 +20,20 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
     actions = [make_published, make_draft]
-    
+
+
+# Comment Model:
+@admin.action(description="Mark selected comments as active")
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)     
+
+@admin.action(description="Mark selected comments as deactivated")
+def make_deactivated(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+    actions = [make_active, make_deactivated]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 
 class PostListView(ListView):
@@ -40,9 +40,35 @@ def post_detail(request, year, month, day, post):
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
+
+    # List of active comments for this post:
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    # [CASE] POST a new comment:
+    if (request.method == 'POST'):
+        # A comment was posted:
+        comment_form = CommentForm(data=request.POST)
+        # Comment form is valid:
+        if comment_form.is_valid():
+            # Create Comment object:
+            new_comment = comment_form.save(commit=False)
+            # Assign the current blog post to the comment:
+            new_comment.post = post
+            # Save the comment to the database:
+            new_comment.save()
+    
+    # [CASE] GET request:
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  { 'post': post,
+                    'comments': comments,
+                    'new_comment': new_comment,
+                    'comment_form': comment_form})
 
 
 def post_share(request, post_id):
